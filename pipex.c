40557eb7f5bdde5958bf3f10306862a8f8b1d315
bsomers42 <brittsomers22@gmail.com>
2022-01-31 15:36:03 +0100

    Working now with cmds in C file

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/31 10:30:34 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/01/31 10:30:38 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/01/31 14:17:39 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,49 +17,48 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int fds[2];
-    int	pid1;
-	int pid2;
-    int file_i;
-	int file_i2;
-	int file_o;
-	int file_o2;
-    
-    pipe(fds);
+	int	fds[2];
+	int	pid1;
+	int	pid2;
+	int	file_i;
+	int	file_o;
+
+	if (argc != 5)
+		return (-1);
+	pipe(fds);
 	if (fds < 0)
 		return (1);
-    pid1 = fork();
+	pid1 = fork();
 	if (pid1 < 0)
 		return (1);
-    if (pid1 == 0) //child process 1, handles first cmd
-    {
-   		file_i = open(argv[1], O_RDONLY);
-   		file_i2 = dup2(file_i, STDIN_FILENO);
+	if (pid1 == 0)
+	{
+		file_i = open(argv[1], O_RDONLY);
+		file_i = dup2(file_i, STDIN_FILENO);
 		dup2(fds[1], STDOUT_FILENO);
 		close (fds[0]);
 		close (fds[1]);
-		execlp("grep", "grep", "c", file_i2, NULL);
-        close(file_i);
-    }
-
+		execlp("grep", "grep", "c", file_i, NULL);
+		close(file_i);
+	}
 	pid2 = fork();
 	if (pid2 < 0)
-		return(1);
-	if (pid2 == 0) //child process 2, handles 2nd cmd
+		return (1);
+	if (pid2 == 0)
 	{
 		dup2(fds[0], STDIN_FILENO);
 		close (fds[0]);
 		close (fds[1]);
-		file_o = open(argv[2], O_WRONLY); 
-		file_o2 = dup2(file_o, STDOUT_FILENO); //STDOUT_FILENO equals 1 (the fd for stdout)
+		file_o = open(argv[2], O_WRONLY);
+		file_o = dup2(file_o, STDOUT_FILENO);
 		execlp("wc", "wc", "-l", NULL);
+		close (file_o);
 	}
-
 	close(fds[0]);
 	close(fds[1]);
-
 	waitpid(pid1, NULL, 0);
 	waitpid(pid2, NULL, 0);
+	return (0);
 }
