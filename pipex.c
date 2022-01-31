42b64f4734876d882d7a9b2076b1716b9c87e2ae
bsomers42 <brittsomers22@gmail.com>
2022-01-31 10:31:06 +0100

    Working with cmd's in C file

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   pipex.c                                            :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: bsomers <bsomers@student.42.fr>              +#+                     */
+/*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/01/28 15:31:04 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/01/28 20:46:14 by bsomers       ########   odam.nl         */
+/*   Created: 2022/01/31 10:30:34 by bsomers       #+#    #+#                 */
+/*   Updated: 2022/01/31 10:30:38 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,12 @@
 int main(int argc, char *argv[])
 {
     int fds[2];
-	int stat;
     int	pid1;
 	int pid2;
+    int file_i;
+	int file_i2;
+	int file_o;
+	int file_o2;
     
     pipe(fds);
 	if (fds < 0)
@@ -32,16 +35,13 @@ int main(int argc, char *argv[])
 		return (1);
     if (pid1 == 0) //child process 1, handles first cmd
     {
+   		file_i = open(argv[1], O_RDONLY);
+   		file_i2 = dup2(file_i, STDIN_FILENO);
 		dup2(fds[1], STDOUT_FILENO);
 		close (fds[0]);
 		close (fds[1]);
-		execlp("ping", "ping", "-c", "5", "google.com", NULL);
-       	// fds[0] = open(argv[1], O_RDONLY);
-		// dup2(fds[0], STDIN_FILENO);
-		// close(fds[0]);
-        // close(fds[1]);
-        // if ((execvp(argv[0], &argv[1])) < 0)
-        //     exit(0);
+		execlp("grep", "grep", "c", file_i2, NULL);
+        close(file_i);
     }
 
 	pid2 = fork();
@@ -52,7 +52,9 @@ int main(int argc, char *argv[])
 		dup2(fds[0], STDIN_FILENO);
 		close (fds[0]);
 		close (fds[1]);
-		execlp("grep", "grep", "rtt", NULL);
+		file_o = open(argv[2], O_WRONLY); 
+		file_o2 = dup2(file_o, STDOUT_FILENO); //STDOUT_FILENO equals 1 (the fd for stdout)
+		execlp("wc", "wc", "-l", NULL);
 	}
 
 	close(fds[0]);
@@ -60,17 +62,4 @@ int main(int argc, char *argv[])
 
 	waitpid(pid1, NULL, 0);
 	waitpid(pid2, NULL, 0);
-	// close(fds[0]);
-
-	// fds[1] = open(argv[3], O_WRONLY);
-	// dup2(fds[1], STDOUT_FILENO);
-	// close(fds[1]);
-
-	// printf("Argc: %d", argc);
-
-	// wp = waitpid(cp, &stat, 0);
-	// return wp == cp && WIFEXITED(stat) ? WEXITSTATUS(stat) : -1;
-
 }
-
-//Example to run: input.txt "grep -e 'c'" "wc -l" output.txt
