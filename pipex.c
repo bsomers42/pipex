c3656dcc9e173ba973fc92cbdd5cbeeeda16593b
Britt Somers <77967616+bsomers42@users.noreply.github.com>
2022-02-07 19:18:35 +0100

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -6,11 +6,23 @@
 /*   By: bsomers <bsomers@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/31 10:30:34 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/02/04 12:21:28 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/02/07 14:58:56 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <stdio.h> //remove!!!!
+
+// void if_error()
+// {
+// 	char *warning;
+// 	if (errno != 0)
+// 	{
+// 		warning = strerror(errno);
+// 		ft_putstr_fd(warning, 1);
+// 		exit(errno);
+// 	}
+// }
 
 void	child_process1(t_info *cmd_path, char *argv[], int *fds)
 {
@@ -72,15 +84,19 @@ int	pipex(int argc, char *argv[], char *envp[])
 	i = 0;
 	if (argc != 5)
 	{
-		write(1, "X", 1);
-		return (-1);
+		ft_putstr_fd("Invalid number of arguments!\n", 1);
+		exit(1);
 	}
 	i = find_path(envp);
 	origpath = ft_strtrim(envp[i], "PATH=");
+		//malloc protecten
 	paths = ft_split(origpath, ':'); //aantal paden: 8
+		//malloc protecten
 	free (origpath);
 	cmd_path.cmd1 = ft_split(argv[2], ' ');
+		//malloc protecten!
 	cmd_path.cmd2 = ft_split(argv[3], ' ');
+		//malloc protecten!
 	cmd_path.path1 = make_and_check_paths(paths, cmd_path.cmd1);
 	cmd_path.path2 = make_and_check_paths(paths, cmd_path.cmd2);
 	if (cmd_path.path1 == NULL || cmd_path.path2 == NULL)
