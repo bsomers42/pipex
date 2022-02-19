be15ee258bc50639f204c15a187a1baf50063a2f
Britt Somers <77967616+bsomers42@users.noreply.github.com>
2022-02-19 19:17:05 +0100

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/04 10:09:19 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/02/09 13:20:57 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/02/19 18:36:26 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,19 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <errno.h>
-# include <string.h> //for strerror
 # include "./libft/libft.h"
 
-typedef struct s_info
+typedef struct s_pip
 {
 	char	*path1;
 	char	*path2;
 	char	**cmd1;
 	char	**cmd2;
 	int		here_doc;
-}				t_info;
+}				t_pip;
 
+void	if_error(char *str);
+void	split_cmd_args(t_pip *cmd_path, char *argv[], t_pip *doc);
 char	*make_and_check_paths(char **paths, char **cmd);
 int		find_path(char *envp[]);
 
